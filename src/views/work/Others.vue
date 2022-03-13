c4f9ea3d1e233bd0ed20237fab68867f0ae257eb
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2022-03-13 20:50:01 +0500

    Add details in other projects

--- src/views/work/Others.vue
+++ src/views/work/Others.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <PostLayout
-      :title="title"
-      :description="description"
-    >
+    <PostLayout :title="title" :description="description">
       <PostInfo
         :stack="['Javascript']"
         title="Board"
@@ -11,6 +8,36 @@
         siteURL="https://board.lablnet.com/"
         codeURL="lablnet/board"
       />
+
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        The main objective of this project is to create simple, standalone,
+        flexible and very lightweight board app / PWA in vanilla JavaScript.
+      </p>
+      <h3 class="project-headings mt-5">Features</h3>
+      <p class="text-justify">
+        <ol class="list-disc mx-6">
+          <li>Platform friendly (Mobile and Desktop)</li>
+          <li>Different styling options.</li>
+          <li>Exports options.</li>
+          <li>And more.</li>
+        </ol>
+      </p>
+      <h3 class="project-headings mt-5">Related Projects</h3>
+      <p class="text-justify">
+        <ol class="list-disc mx-6">
+          <li><a class="text-blue-400 underline" target="_blank" href="https://github.com/cs50/draw.cs50.io">https://github.com/cs50/draw.cs50.io</a> (but they are using paperjs and hammerjs lib).</li>
+        </ol>
+      </p>
+
+      <h3 class="project-headings mt-5">Future Thoughts</h3>
+      <p class="text-justify">
+        I would like to continue to work on adding more features to this
+        project, such as a socket for handling live sharing with more than one
+        person, etc.
+      </p>
+
       <hr class="mt-12 mb-4" />
       <PostInfo
         :stack="['Javascript']"
@@ -19,6 +46,18 @@
         siteURL="https://snake.lablnet.com/index.html"
         codeURL="lablnet/snake"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        It is my own implementation of Snake Game in HTML 5 canvas and vanilla js.
+      </p>
+      <h3 class="project-headings mt-5">KNown Issues</h3>
+      <p class="text-justify">
+        <ol class="list-disc mx-6">
+          <li>When game over, it does not play again on click user have to refresh page.</li>
+          <li>Not working properly in mobile.</li>
+        </ol>
+      </p>
       <hr class="mt-12 mb-4" />
       <PostInfo
         :stack="['Javascript']"
@@ -27,14 +66,43 @@
         siteURL="https://tictactoe.lablnet.com/"
         codeURL="lablnet/TicTacToe"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        It is my own implementation of Tic Tac Toe game using Minimax algorithm.
+      </p>
+      <h3 class="project-headings mt-5">KNown Issues</h3>
+      <p class="text-justify">
+        <ol class="list-disc mx-6">
+          <li>When game over, it does not play again on click user have to refresh page.</li>
+          <li>Not working properly in mobile.</li>
+        </ol>
+      </p>
       <hr class="mt-12 mb-4" />
       <PostInfo
-        :stack="['Javascript']"
+        :stack="['Javascript', 'Jquery', 'Firebase']"
         title="LumberJack Game"
         subtitle="December 2019"
         siteURL="https://lablnet.github.io/LumberJack-game/v2/index.html"
         :collabrators="collabrators"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        It was my freelancing project [but the man from Japan contacted me directly] and hired me. The ideas of the game were followed.
+        <i>This game invites players to “plant trees” in a race against a malevolent lumberjack who sought to chop them down. As the time passed it gets faster and harder to click on the sack to "plant trees". Once time ran out, players are presented with a fact about the necessity of trees for a healthy planet.</i>
+      </p>
+      <h3 class="project-headings mt-5">Credits</h3>
+      <p class="text-justify">
+        <ol class="list-disc mx-6">
+          <li>Character: <a class="text-blue-400 underline" target="_blank" href="https://giphy.com/stickers/stem-2w9gmg97uKtbi">https://giphy.com/stickers/stem-2w9gmg97uKtbi</a></li>
+          <li>Tree: <a class="text-blue-400 underline" target="_blank" href="https://www.cleanpng.com/png-computer-icons-acacia-gum-arabic-tree-clip-art-2752802/download-png.html">https://www.cleanpng.com/png-computer-icons-acacia-gum-arabic-tree-clip-art-2752802/download-png.html</a></li>
+          <li>Sack of Seeds: <a class="text-blue-400 underline" target="_blank" href="https://www.clipartwiki.com/iclip/ihbiTih_vector-illustration-of-sack-of-milled-wheat-grain/">https://www.clipartwiki.com/iclip/ihbiTih_vector-illustration-of-sack-of-milled-wheat-grain/</a></li>
+          <li>Background: <a class="text-blue-400 underline" target="_blank" href="https://superawesomevectors.com/vector-desert-landscape-background/">https://superawesomevectors.com/vector-desert-landscape-background/</a></li>
+        </ol>
+      </p>
+      <p class="text-justify text-red-500"><strong>Note:</strong> Since this was a client project, the source code is confidential and the copyright belongs to this client.</p> 
+
       <hr class="mt-12 mb-4" />
       <PostInfo
         :stack="['JavaScript', 'Cordova']"
@@ -43,6 +111,19 @@
         siteURL="https://play.google.com/store/apps/details?id=com.lablnet.easytools"
         codeURL="lablnet/EasyTools-Source"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        This was my first Android application, I created it just for practice.
+      </p>
+      <h3 class="project-headings mt-5">Features</h3>
+      <p class="text-justify">
+        <ol class="list-disc mx-6">
+          <li>Notes and Todos.</li>
+          <li>Calculator and Converter.</li>
+          <li>Color picker, Stopwatch, Timer and Counter.</li>
+        </ol>
+      </p>
       <hr class="mt-12 mb-4" />
       <PostInfo
         :stack="['Dart', 'Flutter']"
@@ -51,14 +132,32 @@
         siteURL="https://play.google.com/store/apps/details?id=prayer_time.lablnet.app"
         codeURL="alphasofthub/prayer_time"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        A Dart Package to find prayers/ramazan timings for your exact location. This is the rewrite version of PHP code provided by <a class="text-blue-400 underline" href="http://praytimes.org/" target="_blank">praytimes.org</a>
+      </p>
       <hr class="mt-12 mb-4" />
       <PostInfo
-        :stack="['Javascript']"
+        :stack="['Vue', 'Javascript']"
         title="Upwork Visualization"
         subtitle="March 2021"
         siteURL="https://lablnet.github.io/upwork_visualization/"
         codeURL="lablnet/upwork_visualization"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        Actually, being a freelancer in Upwork, I made this tool for myself but I publish it for others to take advantage of.
+        <i>Upwork does not show charts of your earnings and stat of total earnings, this project purpose is to visualize graphically so you know how you’re making progress, how much you have earned, how much you spend and how much Upwork has charged you. -14 April, 2021</i>
+      </p>
+      <h3 class="project-headings mt-5">Features</h3>
+      <p class="text-justify">
+        <ol class="list-disc mx-6">
+          <li>Show chart of earnings.</li>
+          <li>No server is involved.</li>
+        </ol>
+      </p>
       <hr class="mt-12 mb-4" />
       <PostInfo
         :stack="['WordPress']"
@@ -67,6 +166,13 @@
         siteURL="https://www.travelwithatifriaz.com"
         :collabrators="collabrators"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        This is a travel agency website created for my social media marketing teacher.
+      </p>
+      <p class="text-justify text-red-500"><strong>Note:</strong> Since this was a client project, the source code is confidential and the copyright belongs to this client.</p> 
+
       <hr class="mt-12 mb-4" />
       <PostInfo
         :stack="['WordPress']"
@@ -75,6 +181,12 @@
         siteURL="https://spendbtc.io"
         :collabrators="collabrators"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        It was my freelancing project [but the man from Japan contacted me directly] and hired me. This is an Amazon gift buying site using various cryptocurrency options. For the first time in this project I have developed a theme and various plugins for WordPress.
+      </p>
+      <p class="text-justify text-red-500"><strong>Note:</strong> Since this was a client project, the source code is confidential and the copyright belongs to this client.</p> 
       <hr class="mt-12 mb-4" />
       <PostInfo
         :stack="['Python', 'Flask', 'Bootstrap']"
@@ -83,14 +195,24 @@
         siteURL="https://lablnet-flask.herokuapp.com"
         :collabrators="collabrators"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+      <p class="text-justify">
+        It was my freelancing project. It is a website backend in Flask, for user login, signup, reset password and calculation. It also has an admin panel for writing blogs and managing users.
+      </p>
+      <p class="text-justify text-red-500"><strong>Note:</strong> Although this is not a client project, the agreement was to make source code confidential, no doubt the partnership was canceled but I still stand by my agreement.</p>
       <hr class="mt-12 mb-4" />
       <PostInfo
-        :stack="['Python', 'Flask', 'Bootstrap']"
-        title="lablnet-flask"
+        :stack="['Javascript']"
+        title="Resume"
         subtitle="May 2021"
         siteURL="https://lablnet.github.io/resume"
         codeURL="alphasofthub/resume"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        The main objective of this project is to easily create a resume on the GitHub page.
+      </p>
       <hr class="mt-12 mb-4" />
       <PostInfo
         :stack="['JavaScript', 'Cordova']"
@@ -99,6 +221,10 @@
         siteURL="https://play.google.com/store/apps/details?id=com.practicalvalue.ainda&hl=en"
         :collabrators="collabrators"
       />
+      <p class="mt-9 text-justify">
+        It was my freelancing project [but the man from Barazil contacted me directly] and hired me. This is an Android application that lets us log in to find the best business partner.
+      </p>
+      <p class="text-justify text-red-500"><strong>Note:</strong> Since this was a client project, the source code is confidential and the copyright belongs to this client.</p> 
       <hr class="mt-12 mb-4" />
       <PostInfo
         :stack="['JavaScript', 'CSS']"
@@ -107,6 +233,12 @@
         siteURL="https://lablnet.github.io/softhubpro/index.html"
         :collabrators="collabrators"
       />
+      <h3 class="project-headings mt-9">Purpose</h3>
+
+      <p class="text-justify">
+        This is a template for a software house realty company, I started it with some partners but our deal was canceled after a few days so this project could not work.
+      </p>
+      <p class="text-justify text-red-500"><strong>Note:</strong> Although this is not a client project, the agreement was to make source code confidential, no doubt the partnership was canceled but I still stand by my agreement.</p> 
     </PostLayout>
   </div>
 </template>
