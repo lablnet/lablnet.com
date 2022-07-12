f179a8ab97cdca64852e2ab3506a02fec1aae6c2
Muhammad Umer Farooq <umer@lablnet.com>
2022-07-12 20:02:26 +0500

    Added fontawesome
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import {themeSetup} from './utils/index';
+
+// styles
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import './assets/css/tailwind.css'
 
 createApp(App).use(store).use(router).mount('#app')
