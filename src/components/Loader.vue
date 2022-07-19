a0b1ae6496398c58ec095fb62f95a7a846fca8ab
Muhammad Umer Farooq <umer@lablnet.com>
2022-07-19 14:07:37 +0500

    '<script lang=js>' should be above '<style scoped>' on line 12
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -2,13 +2,29 @@
   <div v-if="loading">
     <div class="flex justify-center items-center">
       <div class="loader bg-transparent p-5 rounded-full flex space-x-3">
-        <div class="w-5 h-5 bg-black dark:bg-gray-300 rounded-full animate-bounce"></div>
-        <div class="w-5 h-5 bg-black dark:bg-gray-300 rounded-full animate-bounce"></div>
-        <div class="w-5 h-5 bg-black dark:bg-gray-300 rounded-full animate-bounce"></div>
+        <div
+          class="w-5 h-5 bg-black dark:bg-gray-300 rounded-full animate-bounce"
+        ></div>
+        <div
+          class="w-5 h-5 bg-black dark:bg-gray-300 rounded-full animate-bounce"
+        ></div>
+        <div
+          class="w-5 h-5 bg-black dark:bg-gray-300 rounded-full animate-bounce"
+        ></div>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="js">
+export default {
+  name: "Loader",
+  props: {
+    loading: Boolean,
+  }
+}
+</script>
+
 <style scoped>
 .loader div {
   animation-duration: 0.5s;
@@ -23,12 +39,3 @@
   animation-delay: 0.6s;
 }
 </style>
-
-<script lang="js">
-export default {
-  name: "Loader",
-  props: {
-    loading: Boolean,
-  }
-}
-</script>
