e5da4f064e199609000335ae4f7ac33a567f8254
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2022-07-19 10:05:11 +0500

    refactor
    Signed-off-by: Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -4,26 +4,12 @@ import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      
-    },
-    registered () {
-      
-    },
-    cached () {
-      
-    },
-    updatefound () {
-      
-    },
-    updated () {
-      
-    },
-    offline () {
-      
-    },
-    error (error) {
-      
-    }
+    ready () {},
+    registered () {},
+    cached () {},
+    updatefound () {},
+    updated () {},
+    offline () {},
+    error (error) {}
   })
 }
