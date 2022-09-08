3748a843655c9716e3779fd9a9e2e653f2cb2e11
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2022-09-08 14:40:03 +0000

    Add todo
    Signed-off-by: GitHub <noreply@github.com>

--- src/views/work/FreelancingView.vue
+++ src/views/work/FreelancingView.vue
@@ -15,6 +15,31 @@ import PostLayout from "@/components/PostLayout";
 //import PostInfo from "@/components/PostInfo";
 import { useThemeStore } from '@/store/ThemeStore';
 
+// TODO
+
+// * Upwork Total 40 projects
+// Alert System.
+// EmCasa
+// Hotel Management System
+// ProtonCash, Weareappointments.
+// PHP api intregration form ExpressJs.
+// API testing.
+// Migrate PHP 5.6 code to 7.
+// Integrating curl command into website
+// PHP web app.
+// Python and Flask consulting
+// Set up shopping cart using Stripe.com Checkout (PhP)
+// Software Quality + javaScript game.
+// PHP Quiz App, Employee Management System.
+// * Freelaner total 1 project
+// Php out mysql data
+// * Fiverr total 7 projects
+// Bkash payment woocommerce plugin.
+// * Direct client 3 projects
+// SpendBTC
+// Js tree cutting game.
+// PraticalValue.
+
 export default {
   name: "ZestView",
   components: {
