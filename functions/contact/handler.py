ae6b052a539a00856f140f04cca62dc17169ed76
Muhammad Umer Farooq <umer@lablnet.com>
2022-09-17 10:45:51 +0500

    refactor
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- functions/contact/handler.py
+++ functions/contact/handler.py
@@ -1,41 +1,36 @@
-import json
 import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import re
 import urllib.request
+import json
+import os
+import html
 
 
-def sendEmail(receiver: str, subject: str, message: str, user_email: str, sent_to: str = 'lablnet') -> bool:
+def sanitizeInput(input: str) -> str:
     """
-    Send email.
-    :param receiver: Receiver email.
-    :param subject: Email subject.
-    :param message: Email message.
-    :param user_email: User email.
-    :return: Boolean
-    :author: Muhammad Umer Farooq
+    sanitizes input
+    :param input: Input
+    :return: Sanitized input
+    :author Muhammad Umer Farooq
     """
-    s = smtplib.SMTP(host=os.environ.get("SMTP_HOST"),
-                     port=os.environ.get("SMTP_PORT"))
-    s.starttls()
-    s.login(os.environ.get("SMTP_USER"), os.environ.get("SMTP_PASS"))
-    msg = MIMEMultipart()
-    # Add reply to header.
-    msg.add_header('reply-to', user_email if sent_to ==
-                   'lablnet' else os.environ.get("SMTP_CC"))
+    return html.escape(input if not None else "")
 
-    if sent_to == 'lablnet':
-        # Add CC header.
-        msg.add_header('cc', os.environ.get("SMTP_CC"))
 
-    msg['From'] = os.environ.get("SMTP_USER")
-    msg['To'] = receiver
-    msg['Subject'] = subject
-    msg.attach(MIMEText(message, 'html'))
-    s.send_message(msg)
-    return s
+def validateEmail(email: str) -> bool:
+    """
+    checks if the email is valid or not.
+    :param email: Given email.
+    :return: boolean
+    :author Muhammad Umer Farooq
+    """
+    import re
+    if (re.search('^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$', email)):
+        return True
+    else:
+        return False
 
 
 def response(statusCode: int, errors: dict = None) -> json:
@@ -96,7 +91,7 @@ def validate(data: dict) -> dict:
         if value is None or value == "":
             errors[key] = "This field is required."
     # Check email is valid.
-    if not errors.get('email') and not re.match("^[^@]+@[^@]+\.[^@.]+$", data['email']):
+    if not errors.get('email') and not validateEmail(data['email']):
         errors["email"] = "Please enter a valid email address."
     # Check captcha is valid.
     if not errors and not validateCaptcha(data['captcha']):
@@ -105,21 +100,52 @@ def validate(data: dict) -> dict:
     return errors
 
 
+def sendEmail(receiver: str, subject: str, message: str, user_email: str, sent_to: str = 'lablnet') -> bool:
+    """
+    Send email.
+    :param receiver: Receiver email.
+    :param subject: Email subject.
+    :param message: Email message.
+    :param user_email: User email.
+    :return: Boolean
+    :author: Muhammad Umer Farooq
+    """
+    s = smtplib.SMTP(host=os.environ.get("SMTP_HOST"),
+                     port=os.environ.get("SMTP_PORT"))
+    s.starttls()
+    s.login(os.environ.get("SMTP_USER"), os.environ.get("SMTP_PASS"))
+    msg = MIMEMultipart()
+    # Add reply to header.
+    msg.add_header('reply-to', user_email if sent_to ==
+                   'lablnet' else os.environ.get("SMTP_CC"))
+
+    if sent_to == 'lablnet':
+        # Add CC header.
+        msg.add_header('cc', os.environ.get("SMTP_CC"))
+
+    msg['From'] = os.environ.get("SMTP_USER")
+    msg['To'] = receiver
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'html'))
+    s.send_message(msg)
+    return s
+
+
 def lambda_handler(event, context):
     """
     Lambda Handler.
     """
     # Sent report to cloudwatch.
-    print(event)
+    # print(event)
 
     body = event
 
     data = {
-        'name': body.get('name'),
-        'email': body.get('email'),
-        'subject': body.get('subject'),
-        'message': body.get('message'),
-        'captcha': body.get('captcha'),
+        'name': sanitizeInput(body.get('name')),
+        'email': sanitizeInput(body.get('email')),
+        'subject': sanitizeInput(body.get('subject')),
+        'message': sanitizeInput(body.get('message')),
+        'captcha': sanitizeInput(body.get('captcha')),
     }
 
     # validate the data.
