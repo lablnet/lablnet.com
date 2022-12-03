d81d7a322413bf99aa3622f131bac071e16cf210
Muhammad Umer Farooq <umer@lablnet.com>
2022-12-03 20:10:54 +0500

    update and added more Upwork Projects
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/views/work/FreelancingView.vue
+++ src/views/work/FreelancingView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- Post Layout Start -->
     <PostLayout
       :title="title"
       :description="description"
@@ -9,14 +10,50 @@
           : require('../../assets/images/freelancing.png')
       "
     >
-      <h2 class="text-2xl font-bold mb-4">Upwork</h2>
+      <!-- Post Content Upwork Start -->
+      <h2 class="!text-4xl">Upwork</h2>
       <p class="text-justify dark:text-gray-300">
-        I have been working on Upwork for the past 2 years. I've completed over
-        40 projects and have a 5-star reviews on 90% of my projects. I am not
-        going to list all of my projects here, but I will list some of the
-        projects that I am most proud of.
+        As a freelancer on Upwork for the past two years, I have successfully
+        completed over 40 projects and have received a 5-star rating on 90% of
+        them. While I am unable to list all of my projects, I am proud to
+        highlight a few of my most notable achievements.
       </p>
 
+      <hr class="mt-9 mb-4 mx-auto !w-50" style="width: 50%" />
+
+      <!-- Project 1 Start -->
+      <PostInfo
+        :stack="['PHP', 'HTML', 'CSS']"
+        title="Set up shopping cart using Stripe.com Checkout (PhP)"
+        subtitle="Jan 28, 2021 - Feb 24, 2021"
+        siteURL="https://www.bungalowsoftware.com/subscription/buy/"
+        :collabrators="collabrators"
+      />
+      <h3 class="project-headings mt-9 dark:text-gray-300">Overview</h3>
+      <p class="text-justify dark:text-gray-300">
+        I found this project to be particularly engaging and was able to
+        complete it within seven days (The remaining time was spent on testing
+        by the client). <br />
+        The project description was as follow: <br />
+        <i
+          >Our subscription software service offers customers the ability to
+          choose from five products for their subscription. We currently utilize
+          CheddarGetter for payment management, customer email communications,
+          and other necessary tasks. However, we are in the process of migrating
+          to Stripe.com's subscription billing service and transitioning our
+          code from Asp.net to PHP</i
+        >
+        <br />
+        <br />
+        Beside above description, I also had to create a new database table to
+        store the customer's subscription information, with simple admin panel
+        to manage the subscriptions. <br />
+      </p>
+
+      <hr class="mt-12 mb-4" />
+      <!-- Project 1 End -->
+
+      <!-- Project 2 Start -->
       <PostInfo
         :stack="['Laravel', 'Vue', 'TailwindCSS', 'MySQL', 'PHP']"
         title="Web based Student Information System"
@@ -26,14 +63,19 @@
       />
       <h3 class="project-headings mt-9 dark:text-gray-300">Overview</h3>
       <p class="text-justify dark:text-gray-300">
-        This is a web based student information system that I built for a
-        school. It is a full fledged system that allows the school to manage
-        their students, teachers, classes, and more.
+        I developed a web-based student information system for a school. The
+        system provides comprehensive management capabilities for students,
+        teachers, classes, and other relevant information.
       </p>
 
       <hr class="mt-12 mb-4" />
+      <!-- Project 2 End -->
 
+      <!-- Post Content Upwork End -->
+
+      <!-- Post Content Others Start -->
     </PostLayout>
+    <!-- Post Layout End -->
   </div>
 </template>
 
@@ -56,7 +98,6 @@ import { useThemeStore } from '@/store/ThemeStore';
 // Integrating curl command into website
 // PHP web app.
 // Python and Flask consulting
-// Set up shopping cart using Stripe.com Checkout (PhP)
 // Software Quality + javaScript game.
 // PHP Quiz App, Employee Management System.
 // * Freelaner total 1 project
@@ -84,7 +125,7 @@ export default {
         }
       ],
       title: "Freelancing",
-      description: "These are the best projects I've worked on as a freelancer.",
+      description: "These are among the most notable projects I have had the pleasure of working on as a freelancer.",
     }
   },
   computed: {
