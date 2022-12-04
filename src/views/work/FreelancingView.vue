614eef358c3fbba2c6852c670edc379a77297ea7
Muhammad Umer Farooq <umer@lablnet.com>
2022-12-04 17:00:05 +0500

    update
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/views/work/FreelancingView.vue
+++ src/views/work/FreelancingView.vue
@@ -144,25 +144,13 @@ import { useThemeStore } from '@/store/ThemeStore';
 
 // * Upwork Total 40 projects
 // Alert System.
-// EmCasa
-// Hotel Management System
 // ProtonCash, Weareappointments.
-// PHP api intregration form ExpressJs.
-// API testing.
-// Migrate PHP 5.6 code to 7.
-// Integrating curl command into website
-// PHP web app.
 // Python and Flask consulting
 // Software Quality + javaScript game.
-// PHP Quiz App, Employee Management System.
 // * Freelaner total 1 project
 // Php out mysql data
 // * Fiverr total 7 projects
 // Bkash payment woocommerce plugin.
-// * Direct client 3 projects
-// SpendBTC
-// Js tree cutting game.
-// PraticalValue.
 
 export default {
   name: "ZestView",
