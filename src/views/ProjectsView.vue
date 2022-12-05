1c1dc246e6d11ab96175f54971b5dcb95e2b0a95
Muhammad Umer Farooq <umer@lablnet.com>
2022-12-05 09:28:48 +0500

    added more project
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -70,7 +70,7 @@ export default {
         },
         {
           name: 'Freelancing.',
-          description: "These are the best projects I've worked on as a freelancer.",
+          description: "As a freelancer, I have had the privilege of working on a wide range of fascinating projects. This has allowed me to gain a wealth of experience and expertise in various fields, and has given me the opportunity to work with clients from all over the world. Overall, my experiences as a freelancer have been incredibly rewarding, and I have enjoyed the flexibility and autonomy that comes with this type of work.",
           image: require('../assets/images/freelancing.png'),
           link: 'freelancing',
         }
