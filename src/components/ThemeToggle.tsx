14201b652910d3054940b8c8ec89624e076ec339
Muhammad Umer Farooq <umer@lablnet.com>
2024-01-25 19:08:34 +0500

    add theme changer
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "preact/hooks";
+import type { FunctionalComponent } from "preact";
+
+export default function ThemeToggle(): FunctionalComponent {
+    const [theme, setTheme] = useState('light');
+
+    const handleClick = () => {
+            console.log ("handle click", theme)
+            let _theme = theme === "light" ? "dark" : "light";
+            setTheme(_theme)
+            if (_theme === "dark") {
+                document.documentElement.classList.add("dark");
+                document.documentElement.classList.remove("light");
+            } else {
+                document.documentElement.classList.remove("dark");
+                document.documentElement.classList.add("light");
+            }
+            localStorage.setItem("theme", _theme);
+            console.log ("setting the thenme", _theme)
+    };
+
+    useEffect(() => {
+        setTheme(localStorage.getItem("theme") || "light");
+        if (theme === "dark") {
+        document.documentElement.classList.add("dark");
+        document.documentElement.classList.remove("light");
+        } else {
+        document.documentElement.classList.remove("dark");
+            document.documentElement.classList.add("light");
+        }
+        localStorage.setItem("theme", theme);
+    }, []);
+
+
+    let icon = theme === "light" ? (
+        <i
+      
+        class="
+          text-liText-ternary-dark
+          hover:text-gray-400
+          dark:text-liText-ternary-light dark:hover:text-liBorder-primary-light
+          w-5
+        "
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-moon"
+        >
+          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg></i>) :
+          (
+            <i class="text-gray-200 hover:text-gray-50 w-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-sun"
+            >
+              <circle cx="12" cy="12" r="5"></circle>
+              <line x1="12" y1="1" x2="12" y2="3"></line>
+              <line x1="12" y1="21" x2="12" y2="23"></line>
+              <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+              <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+              <line x1="1" y1="12" x2="3" y2="12"></line>
+              <line x1="21" y1="12" x2="23" y2="12"></line>
+              <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+              <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg></i>
+          )
+    return (
+        <div>
+            <a href="#" onClick={handleClick}  class="py-1">
+                {icon}
+            </a>
+        </div>
+    );
+}
