f71fd39047225ec2174bed76be6fbc54c8598b47
Muhammad Umer Farooq <umer@lablnet.com>
2024-01-26 19:41:00 +0500

    updatE
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/components/LoaderComp/LoaderComp.tsx
@@ -0,0 +1,19 @@
+import { h } from 'preact';
+
+const LoaderComp = ({ loading }) => {
+    if (!loading) {
+        return null;
+    }
+
+    return (
+        <div class="flex justify-center items-center">
+            <div class="loader bg-transparent p-5 rounded-full flex space-x-3">
+                <div class="w-5 h-5 bg-black dark:bg-gray-300 rounded-full animate-bounce"></div>
+                <div class="w-5 h-5 bg-black dark:bg-gray-300 rounded-full animate-bounce"></div>
+                <div class="w-5 h-5 bg-black dark:bg-gray-300 rounded-full animate-bounce"></div>
+            </div>
+        </div>
+    );
+}
+
+export default LoaderComp;
