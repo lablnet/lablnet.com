d0d628f42fa1cd2bff67cfe25470677bb100b8ce
Muhammad Umer Farooq <umer@lablnet.com>
2024-01-26 11:09:50 +0500

    intregrate vuejs
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import vue from "@astrojs/vue";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), vue()]
 });
