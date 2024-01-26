d42a6c156533c30de6f7d262fd3fb5a6ae48867d
Muhammad Umer Farooq <umer@lablnet.com>
2024-01-26 08:12:06 +0500

    add certificate screen
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/content/certificates/cryptography.md
@@ -0,0 +1,8 @@
+---
+title: Cryptography 1
+institute: Coursera online course (Stanford)
+completed: Sep 15, 2022
+picture: ../assets/images/certificates/Coursera 5DZ79VRJSBK3-1.jpg
+link: https://www.coursera.org/account/accomplishments/certificate/5DZ79VRJSBK3
+step: 3
+---
--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,21 @@ const educationCollections = defineCollection({
     }),
 });
 
+// Certificate Collection.
+const certificateCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      institute: z.string(),
+      completed: z.string(),
+      picture: z.string(),
+      link: z.string(),
+      step: z.number(),
+    }),
+});
+
+
 export const collections = {
     'educations': educationCollections,
+    'certificates': certificateCollection,
 };
