5c834d5497d315a73a1abd762782e935a41042b7
Muhammad Umer Farooq <umer@lablnet.com>
2024-01-28 09:07:41 +0500

    Add AstroPWA integration and PWA related changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,36 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import vue from "@astrojs/vue";
+import AstroPWA from '@vite-pwa/astro'
+import manifest from "./public/favicons/site.webmanifest"
 
 export default defineConfig({
   integrations: [
     preact(),
     vue(),
 
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      registerType: 'autoUpdate',
+      manifest,
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      }
+    }),
   ],
+
+  vite: {
+		plugins: [
+		]
+	}
 });
