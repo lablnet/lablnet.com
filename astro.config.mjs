c98325782125a849e93e57b7d1f6a8fea19838de
Muhammad Umer Farooq <umer@lablnet.com>
2024-01-28 08:59:21 +0500

    Update Astro configuration and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,11 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import vue from "@astrojs/vue";
-import { VitePWA } from "vite-plugin-pwa"
-import manifest from "./public/favicons/site.webmanifest"
 
 export default defineConfig({
-  integrations: [preact(), vue()],
+  integrations: [
+    preact(),
+    vue(),
 
-  vite: {
-		plugins: [
-			VitePWA({
-				registerType: "autoUpdate",
-				manifest,
-				workbox: {
-				  globDirectory: 'dist',
-				  globPatterns: [
-				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-				  ],
-				  // Don't fallback on document based (e.g. `/some-page`) requests
-				  // This removes an errant console.log message from showing up.
-				  navigateFallback: null,
-				},
-			})
-		]
-	}
+  ],
 });
