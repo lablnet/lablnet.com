4cee3be1f8120c568890208e12685265171a1282
Muhammad Umer Farooq <umer@lablnet.com>
2024-01-30 12:47:14 +0500

    Update Skills component with dynamic title and subtitle

---
+++ src/utils/const.ts
@@ -0,0 +1,6 @@
+export const meta = {
+    skills: {
+        title: 'Skills',
+        subtitle: 'As a student, I am constantly learning and expanding my knowledge. Some of the skills \n I have developed include:',
+    }
+}
