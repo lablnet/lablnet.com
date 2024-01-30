c6cc4ff539294a8dc139a41faaf4afa779bb650b
Muhammad Umer Farooq <umer@lablnet.com>
2024-01-30 12:56:57 +0500

    Update meta data in Astro components

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,26 @@
 export const meta = {
+    company: {
+        title: "Where I’ve worked.",
+        description: "",
+    },
+    topProjects: {
+        title: "Top Projects.",
+        description: ""
+    },
+    projects:  {
+        title: "Things I've been working on.",
+        description: "I've been working on a lot of things over the years. Here are some of the things I've been working on recently."
+    },
     skills: {
-        title: 'Skills',
-        subtitle: 'As a student, I am constantly learning and expanding my knowledge. Some of the skills \n I have developed include:',
-    }
+        title: 'Skills.',
+        description: 'As a student, I am constantly learning and expanding my knowledge. Some of the skills \n I have developed include:',
+    },
+    certificate: {
+        title: 'Certificates.',
+        description: 'I enjoy facing challenges that are given by my teachers. Here is my educational background',
+    },
+    education: {
+        title: 'Education.',
+        description: 'I enjoy facing challenges that are given by my teachers. Here is my educational background.',
+    },
 }
