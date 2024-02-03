b61c2b8106ccb6143e77019303a2d2de8fb814a8
Muhammad Umer Farooq <umer@lablnet.com>
2024-02-03 16:27:37 +0500

    refactor
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -8,7 +8,7 @@ export const meta = {
         description: ""
     },
     projects:  {
-        title: "Things I've been working on.",
+        title: "Things I've been worki`ng on.",
         description: ""
     },
     skills: {
@@ -24,3 +24,48 @@ export const meta = {
         description: 'I enjoy facing challenges that are given by my teachers. Here is my educational background.',
     },
 }
+
+export const data = [
+	[
+		{
+			id: "workexp",
+			key: 'work',
+			title: 'Work Experience',
+			component: 'Companies'
+		},
+		{
+			id: "workexp",
+			key: 'skills',
+			title: 'Skills',
+			component: 'Skills'
+		}
+	],
+	[
+		{
+			id: "projects",
+			key: "recent",
+			title: "Recent Projects",
+			component: "RecentProjects"
+		},
+		{
+			id: "projects",
+			key: "top",
+			title: "Top Projects",
+			component: "Projects"
+		}
+	],
+	[
+		{
+			id: "educert",
+			key: "education",
+			title: "Education",
+			component: "Education"
+		},
+		{
+			id: "educert",
+			key: "certifications",
+			title: "Certifications",
+			component: "Certificate"
+		}
+	]
+]
