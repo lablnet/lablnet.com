36d1951dc747802296674a481e85492bcfe61d0a
Muhammad Umer Farooq <umer@lablnet.com>
2024-03-07 18:06:09 +0500

    Update
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- src/components/ModelComp.vue
+++ src/components/ModelComp.vue
@@ -1,14 +1,14 @@
 <template>
   <transition name="modal">
     <div
-      class="fixed z-10 inset-0 overflow-y-auto"
+      class="fixed top-16 z-10 inset-0 h-auto overflow-y-auto"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
       @click="$emit('close')"
     >
       <div
-        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+        class="flex items-end justify-center pt-4 px-4 pb-20 text-center sm:block sm:p-0"
         @click.stop
       >
         <div
@@ -21,7 +21,7 @@
           >&#8203;</span
         >
         <div
-          class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          class="inline-block align-top bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all "
         >
           <slot></slot>
         </div>
