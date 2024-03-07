7f977087c05d5b24e899d717330fb8aeb60bb2e9
Muhammad Umer Farooq <umer@lablnet.com>
2024-03-07 16:47:17 +0500

    Added Model Comp
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ src/components/ModelComp.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="modal">
+    <div
+      class="fixed z-10 inset-0 overflow-y-auto"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+      @click="$emit('close')"
+    >
+      <div
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+        @click.stop
+      >
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+        ></div>
+        <span
+          class="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+          >&#8203;</span
+        >
+        <div
+          class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        >
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+};
+</script>
