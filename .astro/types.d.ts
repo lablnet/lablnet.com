b3e8feac3b1e6220fe3f59eca76f73e18b0f4a4a
Muhammad Umer Farooq <umer@lablnet.com>
2024-03-07 14:20:15 +0500

    If project is draft do not show that to homepage
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -180,21 +180,21 @@ declare module 'astro:content' {
   slug: "cryptography";
   body: string;
   collection: "certificates";
-  data: InferEntrySchema<"certificates">
+  data: any
 } & { render(): Render[".md"] };
 "opensource.md": {
 	id: "opensource.md";
   slug: "opensource";
   body: string;
   collection: "certificates";
-  data: InferEntrySchema<"certificates">
+  data: any
 } & { render(): Render[".md"] };
 "python.md": {
 	id: "python.md";
   slug: "python";
   body: string;
   collection: "certificates";
-  data: InferEntrySchema<"certificates">
+  data: any
 } & { render(): Render[".md"] };
 };
 "companies": {
@@ -203,63 +203,63 @@ declare module 'astro:content' {
   slug: "alphasofthub";
   body: string;
   collection: "companies";
-  data: InferEntrySchema<"companies">
+  data: any
 } & { render(): Render[".md"] };
 "alphaz.md": {
 	id: "alphaz.md";
   slug: "alphaz";
   body: string;
   collection: "companies";
-  data: InferEntrySchema<"companies">
+  data: any
 } & { render(): Render[".md"] };
 "direct.md": {
 	id: "direct.md";
   slug: "direct";
   body: string;
   collection: "companies";
-  data: InferEntrySchema<"companies">
+  data: any
 } & { render(): Render[".md"] };
 "fiverr.md": {
 	id: "fiverr.md";
   slug: "fiverr";
   body: string;
   collection: "companies";
-  data: InferEntrySchema<"companies">
+  data: any
 } & { render(): Render[".md"] };
 "mennr.md": {
 	id: "mennr.md";
   slug: "mennr";
   body: string;
   collection: "companies";
-  data: InferEntrySchema<"companies">
+  data: any
 } & { render(): Render[".md"] };
 "projects.md": {
 	id: "projects.md";
   slug: "projects";
   body: string;
   collection: "companies";
-  data: InferEntrySchema<"companies">
+  data: any
 } & { render(): Render[".md"] };
 "resourcesr.md": {
 	id: "resourcesr.md";
   slug: "resourcesr";
   body: string;
   collection: "companies";
-  data: InferEntrySchema<"companies">
+  data: any
 } & { render(): Render[".md"] };
 "riphah.md": {
 	id: "riphah.md";
   slug: "riphah";
   body: string;
   collection: "companies";
-  data: InferEntrySchema<"companies">
+  data: any
 } & { render(): Render[".md"] };
 "upwork.md": {
 	id: "upwork.md";
   slug: "upwork";
   body: string;
   collection: "companies";
-  data: InferEntrySchema<"companies">
+  data: any
 } & { render(): Render[".md"] };
 };
 "educations": {
@@ -268,21 +268,21 @@ declare module 'astro:content' {
   slug: "bachelor";
   body: string;
   collection: "educations";
-  data: InferEntrySchema<"educations">
+  data: any
 } & { render(): Render[".md"] };
 "college.md": {
 	id: "college.md";
   slug: "college";
   body: string;
   collection: "educations";
-  data: InferEntrySchema<"educations">
+  data: any
 } & { render(): Render[".md"] };
 "school.md": {
 	id: "school.md";
   slug: "school";
   body: string;
   collection: "educations";
-  data: InferEntrySchema<"educations">
+  data: any
 } & { render(): Render[".md"] };
 };
 "projects": {
@@ -291,273 +291,273 @@ declare module 'astro:content' {
   slug: "alphaz";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "alphaz_com.md": {
 	id: "alphaz_com.md";
   slug: "alphaz_com";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "alphaz_framework.md": {
 	id: "alphaz_framework.md";
   slug: "alphaz_framework";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "alphaz_website.md": {
 	id: "alphaz_website.md";
   slug: "alphaz_web";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "ash_alphasofthub.md": {
 	id: "ash_alphasofthub.md";
   slug: "alphasofthub_ash";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "ash_covid19.md": {
 	id: "ash_covid19.md";
   slug: "covid19_ash";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "ash_oldsite.md": {
 	id: "ash_oldsite.md";
   slug: "ash_oldsite";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "ash_validity.md": {
 	id: "ash_validity.md";
   slug: "validity_ash";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "ash_webharvest.md": {
 	id: "ash_webharvest.md";
   slug: "webharvest";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "direct_ lumberjackgame.md": {
 	id: "direct_ lumberjackgame.md";
   slug: "direct_lumberjackgame";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "direct_ainda.md": {
 	id: "direct_ainda.md";
   slug: "direct_ainda";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "direct_spendbtc.md": {
 	id: "direct_spendbtc.md";
   slug: "direct_spendbtc";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "fiverr_bkash.md": {
 	id: "fiverr_bkash.md";
   slug: "fiverr_bkash";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "freelance_php.md": {
 	id: "freelance_php.md";
   slug: "freelance_php";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "mennr_crunchbase.md": {
 	id: "mennr_crunchbase.md";
   slug: "mennr_crunchbase";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "others_board.md": {
 	id: "others_board.md";
   slug: "board";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "others_easytools.md": {
 	id: "others_easytools.md";
   slug: "easytools";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "others_fontpicker.md": {
 	id: "others_fontpicker.md";
   slug: "fontpicker";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "others_prayertimes.md": {
 	id: "others_prayertimes.md";
   slug: "prayers_time";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "others_resume.md": {
 	id: "others_resume.md";
   slug: "resume";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "others_snake.md": {
 	id: "others_snake.md";
   slug: "snack";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "others_tictactoe.md": {
 	id: "others_tictactoe.md";
   slug: "tictactoe";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "others_upworkvis.md": {
 	id: "others_upworkvis.md";
   slug: "upwork_visualization";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "others_weather.md": {
 	id: "others_weather.md";
   slug: "weather";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "resourcesr_app.md": {
 	id: "resourcesr_app.md";
   slug: "resourcesr";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "resourcesr_cli.md": {
 	id: "resourcesr_cli.md";
   slug: "resourcesr_cli";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "resourcesr_lite.md": {
 	id: "resourcesr_lite.md";
   slug: "resourcesr_lite";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "resourcesr_web.md": {
 	id: "resourcesr_web.md";
   slug: "resourcesr_web";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "riu_alumni.md": {
 	id: "riu_alumni.md";
   slug: "alumni_riu";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_ pythonconsulting.md": {
 	id: "upwork_ pythonconsulting.md";
   slug: "upwork_pythonconsulting";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_alertsystem.md": {
 	id: "upwork_alertsystem.md";
   slug: "upwork_alertsystem";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_bungalowsoftware.md": {
 	id: "upwork_bungalowsoftware.md";
   slug: "upwork_bungalowsoftware";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_collaboration-app.md": {
 	id: "upwork_collaboration-app.md";
   slug: "collaboration-app";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_democraticai.md": {
 	id: "upwork_democraticai.md";
   slug: "democraticai";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_github_action_app.md": {
 	id: "upwork_github_action_app.md";
   slug: "github-action-and-github-app";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_leadcrm.md": {
 	id: "upwork_leadcrm.md";
   slug: "leadcrm";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_munacjny.md": {
 	id: "upwork_munacjny.md";
   slug: "upwork_munacjny";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_replay.md": {
 	id: "upwork_replay.md";
   slug: "replay-connect";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "upwork_weareappointments.md": {
 	id: "upwork_weareappointments.md";
   slug: "upwork_weareappointments";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 };
 "skills": {
@@ -566,21 +566,21 @@ declare module 'astro:content' {
   slug: "frameworks";
   body: string;
   collection: "skills";
-  data: InferEntrySchema<"skills">
+  data: any
 } & { render(): Render[".md"] };
 "languages.md": {
 	id: "languages.md";
   slug: "languages";
   body: string;
   collection: "skills";
-  data: InferEntrySchema<"skills">
+  data: any
 } & { render(): Render[".md"] };
 "tools.md": {
 	id: "tools.md";
   slug: "tools";
   body: string;
   collection: "skills";
-  data: InferEntrySchema<"skills">
+  data: any
 } & { render(): Render[".md"] };
 };
 
@@ -592,5 +592,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("./../src/content/config.js");
+	export type ContentConfig = never;
 }
--- src/content/projects/ash_webharvest.md
+++ src/content/projects/ash_webharvest.md
@@ -13,6 +13,7 @@ live: https://webharvest.lablnet.com
 github: lablnet/webharvest
 company: alphasofthub
 featured: true
+draft: true
 ---
 
 I am currently working on a project called WebHarvest. WebHarvest is a web scraping tool that allows users to extract data from websites. I am developing this project using Node.js, Express, Vue, and TailwindCSS. I am excited to share more about this project in the future.
