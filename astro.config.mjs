b04a3b74bc841098dcf9168369fd43e5d358cb41
Muhammad Umer Farooq <umer@lablnet.com>
2024-03-07 20:11:06 +0500

    Added Adapter
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import vue from "@astrojs/vue";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
-  integrations: [preact(), vue()]
+  integrations: [preact(), vue()],
+  adapter: cloudflare()
 });
