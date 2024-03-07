b12e205f9202e5cea55296ec32a1c5cd611f0d4c
Muhammad Umer Farooq <umer@lablnet.com>
2024-03-07 20:29:19 +0500

    try fix 2
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import vue from "@astrojs/vue";
 
-// import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [preact(), vue()],
-  //adapter: cloudflare()
+  adapter: cloudflare({ mode: "directory" })
 });
