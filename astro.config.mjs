d9047ed1a98821de3c9692cc40e7e6380c1a3f79
Muhammad Umer Farooq <umer@lablnet.com>
2024-03-08 09:07:31 +0500

    Remove unwanted configuration
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import vue from "@astrojs/vue";
-import cloudflare from "@astrojs/cloudflare";
 
 import node from "@astrojs/node";
 
